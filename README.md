68e2484d21f498b2eb1785b22ab58bc956f4a9a1
5temir <stemir2015@yandex.ru>
2025-04-11 15:26:21 +0400

    Инициализация backend: FastApi, PostgreSQL, Alembic и Docker.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # AI-HR-analytics
 Project for GazpromNeft
+
+## 🚀 Quick Start
+
+To launch the application containers, run the following command:
+
+```bash
+docker compose --env-file ./backend/.env up
+```
+
+## Setting
+
+To start the migration, run the following command:
+
+```bash
+docker-compose exec backend alembic revision --autogenerate -m 'name'
+```
---
+++ backend/src/project/api/healthcheck.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, status
+
+from project.api.depends import database, user_repo
+from project.schemas.healthcheck import HealthCheckSchema
+from project.core.exceptions import DatabaseError
+
+healthcheck_router = APIRouter()
+
+
+@healthcheck_router.get("/healthcheck", response_model=HealthCheckSchema, status_code=status.HTTP_200_OK)
+async def check_health() -> HealthCheckSchema:
+    try:
+        async with database.session() as session:
+            db_is_ok = await user_repo.check_connection(session=session)
+    except DatabaseError as error:
+        db_is_ok = False
+
+    return HealthCheckSchema(
+        db_is_ok=db_is_ok,
+    )
